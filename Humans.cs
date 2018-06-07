ffc2c1bddc2a6f852c2bc2db6224d44d84fe26a3
Kevin Capotosto <foexzya@yahoo.com>
2018-06-06 17:31:46 -0700

    temp

--- Humans.cs
+++ Humans.cs
@@ -1,14 +1,20 @@
+using System;
+using System.Collections.Generic;
 namespace human {
     public class Humans
     {
-        public string Name;
-        public int Strength = 3;
-        public int Intelligence = 3 ;
-        public int Dexterity = 3;
-        public int Health = 100;
-        public Humans(string inp)
+        public string Name = "basic";
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Dexterity { get; set; }
+        public int Health { get; set; }
+        public Humans(string name)
         {
-            this.Name = inp;
+            this.Name = name;
+            this.Strength = 3;
+            this.Intelligence = 3;
+            this.Dexterity = 3;
+            this.Health = 100;
         }
         public Humans(string name, int str, int intl, int dex, int hp)
         {
@@ -22,16 +28,70 @@ namespace human {
         {
             if (trg is Humans)
             {
+                object res = trg.GetType();
                 Humans thing = (Humans)trg;
-                System.Console.WriteLine("You attack "+thing.Name+"!");
+                System.Console.WriteLine(this.Name+" attacks "+thing.Name+"!");
                 int dmg = 5*this.Strength;
                 thing.Health -= dmg;
                 System.Console.WriteLine(thing.Name+" was hit for "+dmg+" hp! They have "+thing.Health+" remaining");
             }
+            if 
             else
             {
                 System.Console.WriteLine("you can't attack that, doofus");
             }
+        }
+    }
+    public class Wizard : Humans
+    {
+        public new int Intelligence = 25;
+        public new int Health = 50;
+        public Wizard(string name) :base(name)
+        {
+            this.Intelligence = 25;
+            this.Health = 50;
+        }
+        public Wizard(string name, int str, int intl, int dex, int hp) : base(name, str, intl, dex, hp)
+        {
+        }
+        public void heal()
+        {
+            int amt = 10 * this.Intelligence;
+            this.Health += amt;
+            System.Console.WriteLine("'Ahh, much better.' Healed for " + amt);
+            System.Console.WriteLine(this.Name+"'s Health is now " + this.Health);
+        }
+        public void fireball(object trg)
+        {
+            if (trg is Humans)
+            {
+                Humans thing = (Humans)trg;
+                System.Console.WriteLine(this.Name+" attacks "+thing.Name+"!");
+                Random rand = new Random();
+                int dmg = rand.Next(20,51);
+                thing.Health -= dmg;
+                System.Console.WriteLine(thing.Name+" was blasted for "+dmg+" hp! They have "+thing.Health+" remaining");
+            }
+            else
+            {
+                System.Console.WriteLine("Woops! Looks like "+this.Name+"'s old age is catching up with him again...");
+            }
+        }
+    }
+    public class Ninja : Humans
+    {
+        public new int Dexterity = 175;
+        public Ninja(string name) :base(name)
+        {
+        }
+        public Ninja(string name, int str, int intl, int dex, int hp) : base(name, str, intl, dex, hp)
+        {
+        }
+        public void steal(object trg)
+        {
+            attack(trg);
+            this.Health += 10;
+            System.Console.WriteLine(this.Name+ " gained 10 health!");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@ namespace human
             int idea = 42;
             Jack.attack(Beefboi);
             Beefboi.attack(idea);
+            Wizard Merazmus = new Wizard("Merazmus");
+            Merazmus.heal();
+            Wizard Abazrafel = new Wizard ("Abazrafel", 2, 2, 2, 40);
+            Abazrafel.fireball(Merazmus);
+            Merazmus.fireball(Abazrafel);
+            Merazmus.fireball(Abazrafel);
+            Ninja Kubo = new Ninja("Kubo");
+            Kubo.steal(Merazmus);
         }
     }
 }
